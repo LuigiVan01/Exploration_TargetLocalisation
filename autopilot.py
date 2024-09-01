f758de90a82ba96039e0cb01044731242b194f41
Rico West <ricardowest1243@gmail.com>
2024-09-01 16:43:49 +1000

    Non-working sort by closest attempt

--- autopilot.py
+++ autopilot.py
@@ -89,7 +89,7 @@ class Autopilot(Node):
                 self.get_logger().info(event.current_status)
                 return
             
-    def frontier_check(self, occupancy_data, random_index):
+    def frontier_check(self, occupancy_data_np, random_index):
         """
         Checks if the point we've selected is on the edge of the frontier, but isn't very close to obstacles
         """
@@ -101,15 +101,15 @@ class Autopilot(Node):
             for y in range(-2,3):
                 row_index = x * self.width + y
                 try:
-                    if occupancy_data.data[random_index + row_index] == -1:
+                    if occupancy_data_np[random_index + row_index] == -1:
                         uncertain_indexes += 1
-                    elif occupancy_data.data[random_index + row_index] > 75:
+                    elif occupancy_data_np[random_index + row_index] > 65:
                         obstacle_indexes += 1
                 #the index of a point next to the random_index may not be within the range of occupancy_data.data, so the IndexError is handled below
                 except IndexError:
                     pass
         #if the point in question (random_index) is next to at least one uncertain_index and not next to between 2 and 4 obstacles, then this is a valid index along the frontier
-        if uncertain_indexes > 3 and 2 < obstacle_indexes < 4:
+        if uncertain_indexes > 1 and 2 < obstacle_indexes:
             return True
         else:
             return False
@@ -133,40 +133,46 @@ class Autopilot(Node):
         isthisagoodwaypoint = False
         minimum_distance = float('inf')
         self.searching_for_waypoint = True
+        occupancy_data_np = np.array(occupancy_data.data)
+        allowable_distance = 125
 
-        while len(occupancy_data.data) > 0:
-            while isthisagoodwaypoint == False:
-                random_index = randrange(len(occupancy_data.data))
-                self.potential_pos = occupancy_data.data[random_index]
-                frontier_detection= self.frontier_check(occupancy_data, random_index)
-                #Remove index from occupancy data so that the same point isn't checked twice
-                #first create boolean mask array that has the same shape as occupancy_data, but is True for indices we want to keep, and false otherwise
-                    #This is just to avoid creating another array without the index we've just checked, because numpy doesn't support in-place deletion of elements in an array.
+        #while occupancy_data_np.size > 0:
+        while isthisagoodwaypoint == False:
+            if occupancy_data_np.size == 0:
+                break
+            random_index = randrange(occupancy_data_np.size)
+            self.potential_pos = occupancy_data_np[random_index]
+            frontier_detection= self.frontier_check(occupancy_data_np, random_index)
+            #Remove index from occupancy data so that the same point isn't checked twice
+            occupancy_data_np_new = np.delete(occupancy_data_np, random_index)
+            occupancy_data_np = occupancy_data_np_new
 
-                mask = np.ones(occupancy_data.data.shape[0], dtype=bool)
-                mask[random_index] = False
+            if self.potential_pos != -1 and self.potential_pos <= 20 and frontier_detection == True:
+                self.get_logger().info('Found Good Point with OccupancyData:')
+                self.get_logger().info(str(self.potential_pos))
+                isthisagoodwaypoint = True
 
-                #Replace tell occupancy_data which indices to keep
-                occupancy_data.data[:] = occupancy_data[mask]
+            else:
+                #self.get_logger().info('Bad Point Finding New')
+                pass
 
-                if self.potential_pos != -1 and self.potential_pos <= 20 and frontier_detection == True:
-                    self.get_logger().info('Found Good Point with OccupancyData:')
-                    self.get_logger().info(str(self.potential_pos))
-                    isthisagoodwaypoint = True
+        row_index = random_index / self.width
+        col_index = random_index % self.width
+        self.get_logger().info('Checking if point is closest')
+        #Find straightline distance between Turtlebot and current point, using pythag
+        distance = math.sqrt((row_index - self.current_position.pose.position.x)**2 + (col_index - self.current_position.pose.position.y)**2)
+            
+            #if distance < minimum_distance and distance > #allowable_distance:
+            #    self.new_waypoint.pose.position.x = (col_index * resolution) + origin_x + (resolution/2)
+            #    self.new_waypoint.pose.position.y = (row_index * resolution) + origin_x + (resolution/2)
+            #    isthisagoodwaypoint = False
+            #    self.get_logger().info('Found a closer point')
+            #    self.get_logger().info(str(distance))
 
-                else:
-                    self.get_logger().info('Bad Point Finding New')
-
-            row_index = random_index / self.width
-            col_index = random_index % self.width
-
-            #Find straightline distance between Turtlebot and current point, using pythag
-            distance = math.sqrt((row_index - self.current_position.x)**2 + (col_index - self.current_position.y)**2)
-
-            if distance < minimum_distance:
-                self.new_waypoint.pose.position.x = (col_index * resolution) + origin_x + (resolution/2)
-                self.new_waypoint.pose.position.y = (row_index * resolution) + origin_x + (resolution/2)
-
+            #else:
+            #    isthisagoodwaypoint = False
+                
+        self.get_logger().info('Found the closest point, and no more points to search. Publishing waypoint...')
         self.waypoint_publisher.publish(self.new_waypoint)
         self.ready = False
 
