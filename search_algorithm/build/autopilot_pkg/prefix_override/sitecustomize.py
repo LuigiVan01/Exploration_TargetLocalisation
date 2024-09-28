a04e4ec15c15be0be988188f77d7efb10afb7acb
LuigiVan01 <luigi.vanacore@mail.polimi.it>
2024-09-28 19:11:48 +1000

    file reorganization 2

--- search_algorithm/build/autopilot_pkg/prefix_override/sitecustomize.py
+++ search_algorithm/build/autopilot_pkg/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/mnt/c/Users/graci/Desktop/search_algorithm/install/autopilot_pkg'
+    sys.prefix = sys.exec_prefix = '/home/lv/Desktop/Git/metr4202_2024_team20/search_algorithm/install/autopilot_pkg'
